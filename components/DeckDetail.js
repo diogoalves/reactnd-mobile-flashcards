690ce088846df43c72290c8bdaa3b5b408283bad
casa <casa@casas-iMac.lan>
2018-07-01 20:02:19 -0300

    adds styling in DeckDetail view

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-import { white, gray } from '../utils/colors';
+import { white, black, gray } from '../utils/colors';
 
 class DeckDetail extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -10,47 +10,83 @@ class DeckDetail extends Component {
     };
   };
   render () {
-    const { title, cardsQuantity } = this.props;
+    const { title, cardsQuantity, goAddCard, goQuiz } = this.props;
     return (
-      <View>
+      <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.subtitle}>{`${cardsQuantity} cards`}</Text>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('NewCard', { entryId: 1 })}>
-          <Text>Add Card</Text>
+        <View style={{padding: 40}} />
+        <TouchableOpacity onPress={goAddCard} style={styles.addCardButton}>
+          <Text style={styles.addCardButtonText}>Create a New Question</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('Quiz', { entryId: 1 })}>
-          <Text>Start Quiz</Text>
-        </TouchableOpacity>        
+        <View style={{padding: 10}} />
+        <TouchableOpacity onPress={goQuiz} style={styles.quizButton}>
+          <Text style={styles.quizButtonText}>Start a Quiz</Text>
+        </TouchableOpacity>       
       </View>
     );
   }
 } 
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    paddingVertical: 30,
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center',
     backgroundColor: white,
-    borderBottomColor: gray,
-    borderBottomWidth: 0.5
+    justifyContent: 'center'
   },
   title: {
     textAlign: 'center',
-    fontSize: 17,
+    fontSize: 40,
   },
   subtitle: {
     textAlign: 'center',
-    color: gray
-
+    color: gray,
+  },
+  addCardButton: {
+    backgroundColor: white,
+    padding: 10,
+    borderRadius: 7,
+    height: 50,
+    width: 200,
+    marginLeft: 40,
+    marginRight: 40,
+    borderWidth: 1
+  },
+  addCardButtonText: {
+    color: gray,
+    fontSize: 17,
+    textAlign: 'center',
+  },
+  quizButton: {
+    backgroundColor: black,
+    padding: 10,
+    borderRadius: 7,
+    height: 50,
+    width: 200,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  quizButtonText: {
+    color: white,
+    fontSize: 17,
+    textAlign: 'center',
   }
 });
 
 const mapStateToProps = (decks, ownProps) => {
-  const deck = decks[ownProps.navigation.getParam('deckId')];
+  const deckId = ownProps.navigation.getParam('deckId');
+  const deck = decks[deckId];
+  const title = deck['title'];
   return ({
-    title: deck['title'],
-    cardsQuantity: deck['questions'].length
+    title: title,
+    cardsQuantity: deck['questions'].length,
+    goAddCard: () => ownProps.navigation.navigate('NewCard', { deckId, title }),
+    goQuiz: () => ownProps.navigation.navigate('Quiz', { deckId })
   })
 };
+
 
 
 export default connect(mapStateToProps)(DeckDetail);
