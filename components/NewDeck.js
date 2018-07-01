8a8baec774410e738c0c9cf34bd1dfdafe79e699
casa <casa@casas-iMac.lan>
2018-07-01 20:03:07 -0300

    adds styling in NewDeck view

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, TextInput, View, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, TextInput, KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { addDeck } from '../redux/actions';
+import { blue, white } from '../utils/colors';
 
 class NewDeck extends Component {
 
@@ -15,20 +16,23 @@ class NewDeck extends Component {
 
   onSubmit = () => {
     this.props.dispatch(addDeck(this.state.newDeckTitle));
-    this.props.navigation.navigate('Decks');
+    this.props.navigation.navigate('DeckDetail', { deckId: this.state.newDeckTitle });
   }
 
   render() {
+    const { newDeckTitle } = this.state;
     return (
-      <View styles={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
         <Text style={styles.title}>What is the title of your new deck?</Text>
         <TextInput style={styles.input}
           placeholder="Deck Title"
           onChangeText={this.handleChange}
-          value={this.state.newDeckTitle}
-          onSubmitEditing={this.onSubmit}
+          value={newDeckTitle}
         />
-      </View>
+        <TouchableOpacity onPress={this.onSubmit} style={styles.addDeckButton} disabled={newDeckTitle.length === 0}>
+          <Text style={styles.addDeckButtonText}>Create Deck</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -37,14 +41,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, 
     justifyContent: 'center', 
-    alignItems: 'center' 
+    alignItems: 'center',
+    backgroundColor: white,
+    justifyContent: 'space-around'
   },
   title: {
     textAlign: 'center',
     fontSize: 40,
   },
   input: {
-    height: 40
+    width: 300,
+    height: 60,
+    borderWidth: 1,
+    padding: 14,
+    borderRadius: 7  
+  },
+  addDeckButton: {
+    backgroundColor: blue,
+    padding: 10,
+    borderRadius: 7,
+    height: 50,
+    width: 200,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  addDeckButtonText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
   }
 })
 
