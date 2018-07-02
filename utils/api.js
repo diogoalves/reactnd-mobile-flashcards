5e66dd9b62643e43dbc893b6ed64af647afe516c
casa <casa@casas-iMac.lan>
2018-07-02 14:49:49 -0300

    refactor a default decks set used during test

--- utils/api.js
+++ utils/api.js
@@ -2,35 +2,9 @@ import { AsyncStorage } from 'react-native';
 
 const STORAGE_KEY = 'reactnd-mobile-flashcards';
 
-const decks = {
-  React: {
-    title: 'React',
-    questions: [
-      {
-        question: 'What is React?',
-        answer: 'A library for managing user interfaces',
-      },
-      {
-        question: 'Where do you make Ajax requests in React?',
-        answer: 'The componentDidMount lifecycle event',
-      },
-    ],
-  },
-  JavaScript: {
-    title: 'JavaScript',
-    questions: [
-      {
-        question: 'What is a closure?',
-        answer:
-          'The combination of a function and the lexical environment within which that function was declared.',
-      },
-    ],
-  },
-};
-
 const getDecks = () => {
   return AsyncStorage.getItem(STORAGE_KEY).then(
-    result => (result ? JSON.parse(result) : decks)
+    result => (result ? JSON.parse(result) : {})
   );
 };
 
