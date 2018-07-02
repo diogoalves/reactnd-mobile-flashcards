6ab9226ca46d09a4a7010ad14eb52decc91fc005
casa <casa@casas-iMac.lan>
2018-07-02 01:00:05 -0300

    add apploading when loading decks

--- components/ListDecks.js
+++ components/ListDecks.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, View, TouchableOpacity, ScrollView, StyleSheet} from 'react-native';
+import { ScrollView, StyleSheet} from 'react-native';
 import ListDeckItem from './ListDeckItem';
 import Api from '../utils/api';
 import { fetchDecksSuccessful } from '../redux/actions';
+import { AppLoading } from 'expo';
+import { white } from '../utils/colors';
 
 class ListDecks extends Component {
-  
+
+  state = {
+    isReady: false
+  }
+
   componentDidMount = () => {
-    Api.getDecks().then(r => this.props.dispatch(fetchDecksSuccessful(r)));
+    Api.getDecks().then(r => {
+      this.props.dispatch(fetchDecksSuccessful(r))
+      this.setState({isReady: true});
+    });
   }
 
   render () {
     const { decks } = this.props;
+    if (!this.state.isReady) {
+      return (
+        <AppLoading />
+      );
+    }
+
     return (
       <ScrollView contentContainerStyle={styles.contentContainer}>
         {Object.keys(decks).map( d => <ListDeckItem key={decks[d].title} title={decks[d].title} cardsQuantity={decks[d].questions.length} onClick={() => this.props.navigation.navigate('DeckDetail', { deckId: decks[d].title })} />)}
@@ -23,7 +38,8 @@ class ListDecks extends Component {
 
 const styles = StyleSheet.create({
   contentContainer: {
-    paddingVertical: 5
+    paddingVertical: 5,
+    backgroundColor: white
   }
 });
 
